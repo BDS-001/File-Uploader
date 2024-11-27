15414056c00bf242b620db184b9c7fab5a683234
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2024-11-27 16:13:47 -0500

    add: header element

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
 const userController = require('../controllers/userController')
+const {requireAuthSignedIn} = require('../middleware/requireAuth')
 
-router.get("/", userController.getHomepage);
+router.get("/", requireAuthSignedIn, userController.getHomepage);
 
 router.get("/signup", userController.getUserSignUp);
 router.post("/signup", userController.postUserSignUp);
