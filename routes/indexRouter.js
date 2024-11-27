71e79f23dd16aa0bc63ab448fdbfa4ff4baef17f
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2024-11-27 15:48:17 -0500

    add: router setup to use controllers

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,7 +1,13 @@
 const router = require("express").Router();
+const userController = require('../controllers/userController')
 
-router.get("/", (req, res) => {
-    res.send("Hello World");
-});
+router.get("/", userController.getHomepage);
+
+router.get("/signup", userController.getUserSignUp);
+router.post("/signup", userController.postUserSignUp);
+
+router.get('/login', userController.getLoginPage)
+router.post("/login", userController.postLogIn);
+router.get("/log-out", userController.getLogOut);
 
 module.exports = router
