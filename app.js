551dc3b3210eda8e42eec4ba781cedf89fe9254f
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2024-12-06 20:08:42 -0500

    add: public folder with basic styling

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const indexRouter = require('./routes/indexRouter');
 // --- View Engine Setup ---
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(express.static('public'));
 
 
 // --- Middleware Setup ---
